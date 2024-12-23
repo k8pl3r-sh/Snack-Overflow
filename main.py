e0f5ec55287d55839da9f04b60e397f06f1cf966
k8pl3r-sh <k8pl3r@gmail.com>
2024-12-23 09:44:47 +0100

    Patch config as parameter

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 import asyncio
 from src.customers import Customer
 from src.restaurant import Restaurant
+from yaml import safe_load
 
 async def main():
-    restaurant = Restaurant()
-    customers = [Customer(f"Customer {i}") for i in range(5)]
+    config = safe_load(open("config.yml"))
+    restaurant = Restaurant(config)
+    customers = [Customer(f"Customer {i}", config) for i in range(5)]
     orders = ["soda", "burger", "fries", "soda", "burger"]
 
     tasks = [customer.place_order(restaurant, order) for customer, order in zip(customers, orders)]
